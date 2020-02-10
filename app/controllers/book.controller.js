c513c8090db663dee718cb8fdc312eb39f71bccf
mnashrullah <muhammadnashrullah.it@gmail.com>
2020-02-10 11:28:08 +0700

    make it better

---
+++ app/controllers/book.controller.js
@@ -0,0 +1,14 @@
+//app/controllers/book.controller.js
+const Book = require("../models/book.model");
+//Mengambil semua data buku
+exports.findAll = (req, res) => {
+  Book.getAll((err, data) => {
+    if (err) {
+      res.status(500).send({
+        message:
+          err.message || "Terjadi kesalahan"
+      });
+    } else res.send(data);
+  });
+};
+
---
+++ app/routes/book.routes.js
@@ -0,0 +1,8 @@
+//app/routes/book.routes.js
+module.exports = app => {
+    const books = require("../controllers/book.controller");
+    // cara mengakses gambar http://polibatam.ac.id/img/perpustakaan.png 
+    // Mengambil semua data
+    app.get("/api/books", books.findAll);
+};
+
